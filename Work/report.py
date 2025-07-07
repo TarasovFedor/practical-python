8375838c3b0afd8d8c5d07d07cc007fa26ff2eef
TarasovFedor <fedor.tarasov.2007@mail.ru>
2025-07-07 21:51:58 +0300

    third day: 2.3 section

--- Work/report.py
+++ Work/report.py
@@ -1,8 +1,9 @@
 # report.py
 #
 # Exercise 2.4
-# from pprint import pprint
 
+from pprint import pprint
+from typing import Any
 import sys
 import os
 other_dir = os.path.join(os.path.dirname(__file__), 'Other')
@@ -10,7 +11,8 @@ sys.path.append(other_dir)
 import read_prices
 
 
-def read_portfolio(filename: str) -> list[dict[str, float]]:
+def read_portfolio(filename: str) -> list[dict[str, Any]]:
+    '''Parces provided portfolio.csv making a list of dictionaries containing the name of the stock, amount of shares of that stock and its price'''
     portfolio = []
     rows = []
 
@@ -33,20 +35,49 @@ def read_portfolio(filename: str) -> list[dict[str, float]]:
 
     return portfolio
 
-# print(read_portfolio('Work/Data/portfolio.csv'))
-# pprint(read_portfolio('Work/Data/portfolio.csv'))
+def make_report(portfolio: list[dict[str, Any]], prices: dict[str, Any]) -> list[tuple[Any]]:
+    '''Makes a report about changes in prices of stocks from provided portfolio'''
+    report = []
+
+    for i in range(len(portfolio)):
+        stock_name = portfolio[i]['name']
+        stock_shares = portfolio[i]['shares']
+        new_price = prices[stock_name]
+        change = new_price - portfolio[i]['price']
+
+        report.append((stock_name, stock_shares, new_price, change))
+
+    return report
+
 
 portfolio = read_portfolio('Work/Data/portfolio.csv')
 prices = read_prices.read_prices('Work/Data/prices.csv')
 
-difference = 0
-for i in range(len(portfolio)):
-    stock_name = portfolio[i]['name']
-    stock_shares = portfolio[i]['shares']
+headers = ('Name', 'Shares', 'Price', 'Change')
+# for h in headers:
+#     print(f'{h:>10s} ', end='')
+# print()
+print(''.join(f'{h:>10s} ' for h in headers))
+print('-'*10 + ' ' + '-'*10 + ' ' + '-'*10 + ' ' + '-'*10)
 
-    spent = portfolio[i]['price'] * stock_shares
-    possible_income = prices[stock_name] * stock_shares # type: ignore
+report = make_report(portfolio, prices)
+for name, shares, price, change in report: #type: ignore
+    print(f'{name:>10s} {shares:>10d} {('$'+str(price)):>10s} {change:>10.2f}')
 
-    difference += possible_income - spent
 
-print(difference)
+# print(read_portfolio('Work/Data/portfolio.csv'))
+# pprint(read_portfolio('Work/Data/portfolio.csv'))
+
+# pprint(make_report(portfolio, prices))
+
+# difference = 0
+# for i in range(len(portfolio)):
+#     stock_name = portfolio[i]['name']
+#     stock_shares = portfolio[i]['shares']
+
+#     spent = portfolio[i]['price'] * stock_shares
+#     possible_income = prices[stock_name] * stock_shares # type: ignore
+
+#     difference += possible_income - spent
+
+# print(difference)
